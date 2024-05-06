b825b72fbf8bfd02bcdb8d04534628f63113dcdc
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-05-06 14:53:44 +0100

    Understood structs for streams

--- src/lectures/demo/structs/structs.go
+++ src/lectures/demo/structs/structs.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
-func main() {
+type Passenger struct {
+	Name         string
+	TicketNumber int
+	Boarded      bool
+}
 
+type Bus struct {
+	FrontSeat Passenger
+}
+
+func main() {
+	said := Passenger{"SAID", 2, false}
+	fmt.Println(said)
+
+	var (
+		bill = Passenger{Name: "Bill", TicketNumber: 3}
+		ella = Passenger{Name: "Ella", TicketNumber: 5}
+	)
+	fmt.Println(bill, ella)
+
+	var techie Passenger
+	techie.Name = "Emma"
+	techie.TicketNumber = 4
+	fmt.Println(techie)
+
+	said.Boarded = true
+	bill.Boarded = true
+
+	if bill.Boarded {
+		fmt.Println(bill.Name, "has boarded the bus")
+	}
+	if said.Boarded {
+		fmt.Println(said.Name, " has boarded the bus")
+	}
+
+	techie.Boarded = true
+	bus := Bus{techie}
+	fmt.Println(bus)
+	fmt.Println(bus.FrontSeat.Name, "Is the one on the front seat")
 }
--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -1,24 +1,56 @@
-//--Summary:
-//  Create a program to calculate the area and perimeter
-//  of a rectangle.
-//
-//--Requirements:
-//* Create a rectangle structure containing a length and width field
-//* Using functions, calculate the area and perimeter of a rectangle,
-//  - Print the results to the terminal
-//  - The functions must use the rectangle structure as the function parameter
-//* After performing the above requirements, double the size
-//  of the existing rectangle and repeat the calculations
-//  - Print the new results to the terminal
-//
-//--Notes:
-//* The area of a rectangle is length*width
-//* The perimeter of a rectangle is the sum of the lengths of all sides
-
 package main
 
 import "fmt"
 
-func main() {
+//--Requirements:
+//--Notes:
+//* The area of a rectangle is length*width
+//* The perimeter of a rectangle is the sum of the lengths of all sides
+type Coordinate struct {
+	x, y int
+}
 
+//* Create a rectangle structure containing a length and width field
+type Rectangle struct {
+	a Coordinate // top left
+	b Coordinate // bottom right
+}
+
+//* Using functions, calculate the area and perimeter of a rectangle,
+func length(rect Rectangle) int {
+	return (rect.a.y - rect.b.y)
+}
+
+func width(rect Rectangle) int {
+	return (rect.b.x - rect.a.x)
+}
+
+func area(rect Rectangle) int {
+	return length(rect) * width(rect)
+}
+
+func perimeter(rect Rectangle) int {
+	// return 2 * (length(rect) + width(rect))
+	return (width(rect) * 2) + (length(rect) * 2)
+}
+
+//  - Print the results to the terminal
+//  - The functions must use the rectangle structure as the function parameter
+func printInfo(rect Rectangle) {
+	fmt.Println("Rectangle: ", rect)
+	fmt.Println("Area is: ", area(rect))
+	fmt.Println("Perimeter is: ", perimeter(rect))
+}
+
+func main() {
+	rect := Rectangle{a: Coordinate{0, 7}, b: Coordinate{10, 0}}
+	//  - Print the results to the terminal
+	printInfo(rect)
+	//* After performing the above requirements, double the size
+	//  of the existing rectangle and repeat the calculations
+	rect.b.x *= 2
+	rect.a.y *= 2
+	printInfo(rect)
+	//  - Print the new results to the terminal
+	//
 }
