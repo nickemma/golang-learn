922ddbec56bbbbbc0ecea0207e34138e1e6f4eb3
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-05-08 09:55:20 +0100

    Did the array exercise

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,6 +2,46 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name    string
+	cleaned bool
+}
 
+func checkCleanLiness(rooms [4]Room) {
+	// when working with a for loop always make sure to make a copy of the array
+	for i := 0; i < len(rooms); i++ {
+		// make a copy of the room here always
+		room := rooms[i]
+		if room.cleaned {
+			fmt.Println(room.name, "is clean")
+		} else {
+			fmt.Println(room.name, "is dirty")
+		}
+	}
+}
+
+func main() {
+	// Create a shopping list array with enough room for 4 or more products
+	myShoppingList := [...]string{"Milk", "Eggs", "Bread"}
+
+	for i := 0; i < len(myShoppingList); i++ {
+		item := myShoppingList[i]
+		fmt.Println(item)
+	}
+
+	rooms := [...]Room{
+		{name: "Office"},
+		{name: "Warehouse"},
+		{name: "Reception"},
+		{name: "Ops"},
+	}
+
+	checkCleanLiness(rooms)
+	fmt.Println("Done! All rooms checked.")
+
+	rooms[1].cleaned = true
+	rooms[3].cleaned = true
+
+	checkCleanLiness(rooms)
+	fmt.Println("Done! All rooms checked.", rooms)
 }
--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -2,6 +2,11 @@
 //  Create a program that can store a shopping list and print
 //  out information about the list.
 //
+
+package main
+
+import "fmt"
+
 //--Requirements:
 //* Using an array, create a shopping list with enough room
 //  for 4 products
@@ -14,10 +19,39 @@
 //* Add a fourth product to the list and print out the
 //  information again
 
-package main
-
-import "fmt"
+// Create a shopping list array with enough room for 4 or more products
+type ShoppingList struct {
+	//  - Products must include the price and the name
+	name  string
+	price float64
+}
 
 func main() {
+	// Insert 3 products into the array
+	products := [4]ShoppingList{
+		{name: "Milk", price: 2.50},
+		{name: "Eggs", price: 1.50},
+		{name: "Bread", price: 1.00},
+	}
 
+	fmt.Println("The last item on the list: ", products[len(products)-2].name)
+	fmt.Println("The total number of items: ", len(products))
+
+	// The total cost of the items
+	totalCost := 0.0
+	for i := 0; i < len(products); i++ {
+		totalCost += products[i].price
+	}
+	fmt.Println("The total cost of the items: ", totalCost)
+
+	// Add a fourth product to the list
+	products[3] = ShoppingList{name: "coffee", price: 1.25}
+
+	// Recalculate the total cost of the items after adding the fourth product
+	totalCost += products[3].price
+
+	// Print out the information again
+	fmt.Println("The last item on the list: ", products[len(products)-1].name)
+	fmt.Println("The total number of items: ", len(products))
+	fmt.Println("The total cost of the items: ", totalCost)
 }
